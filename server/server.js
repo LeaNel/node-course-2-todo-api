75f1cb2e2fb175c52c5d13fe1553d05b39d72848
Leanne Nel <leanne@Leannes-MacBook-Air.local>
2018-02-12 10:40:14 +0200

    Add test cases for GEt/todos:id

--- server/server.js
+++ server/server.js
@@ -32,6 +32,21 @@ app.get('/todos', (req,res) => {
 });
 
 // GET/todos/12345535
+// app.get('/todos/:id' , (req,res) => {
+//     var id = req.params.id;
+//     if (!ObjectID.isValid(id)) {
+//         return res.status(404).send()
+//     };
+//     Todo.findById(id).then((todo) => {
+//         if(!todo) {
+//             return res.status(404).send()
+//         };
+//         return res.send({todo});
+//     },(e) => {
+//         res.status(400).send();
+//     });
+// });
+
 app.get('/todos/:id' , (req,res) => {
     var id = req.params.id;
     if (!ObjectID.isValid(id)) {
@@ -42,7 +57,7 @@ app.get('/todos/:id' , (req,res) => {
             return res.status(404).send()
         };
         return res.send({todo});
-    },(e) => {
+    }).catch((e) => {
         res.status(400).send();
     });
 });
@@ -53,6 +68,4 @@ app.listen(3000, () => {
 
 
 module.exports = {app};
-
-
 
