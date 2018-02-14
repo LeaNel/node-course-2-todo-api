bbaedf674693ef51a600f56ee60f09b1c1f1dc5e
Leanne Nel <leanne@Leannes-MacBook-Air.local>
2018-02-14 08:39:38 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,30 @@
 // const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = { 
-    id:10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// })
 
-console.log(token);
+var hashedPassword = '$2a$10$unSrPAQcjJcJ0syRBf/FS.PgbHhxgVEwAyeZFEdMe.VISTTlXutpe';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res)
+})
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// var data = { 
+//     id:10
+// };
+
+// var token = jwt.sign(data, '123abc');
+
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 
 
